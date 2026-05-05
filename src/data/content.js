f027208e191c38d66f62a78d67e51df75e45023e
heytechie <tanishk.kapoorpvt@gmail.com>
2026-05-05 15:12:05 +0530

    final speaker changes

--- src/data/content.js
+++ src/data/content.js
@@ -2,19 +2,19 @@ export const speakers = [
   { name: "Prof. Chiranjeev Kumar", affiliation: "Professor, IIT(ISM) Dhanbad", image: "/ChiranjeevKumar.jpeg" },
   { name: "Prof. Ranjit Kumar Upadhyay", affiliation: "Professor (HAG) , IIT-ISM Dhanbad", image: "/RanjitKumar.jpeg" },
   { name: "Dr. Rajesh Kumar Pandey", affiliation: "Professor, IIT BHU", image: "/RajeshKumar.jpeg" },
-  { name: "Dr. Jagdish Bansal", affiliation: "South Asian University", image: "/JagdishChand.png" },
+  { name: "Dr. Ruhul Amin", affiliation: "IIIT Nayapura", image: "/ruhulamin.jpg" },
+
   { name: "Dr. Ram Bilas Pachori", affiliation: "IIT Indore", image: "/RamBilas.webp" },
   { name: "Dr. Ashutosh Singh", affiliation: "IIIT Bhopal", image: "/AshutoshKumar.jpg" },
   { name: "Dr. Philip Virgil Astillo", affiliation: "University of San Carlos", image: "/PhilipVirgil.webp" },
-  { name: "Dr. Kapil Ahuja", affiliation: "IIT Indore", image: "/KapilAhuja.jpg" },
+  { name: "Dr. Jagdish Bansal", affiliation: "South Asian University", image: "/JagdishChand.png" },
 
   { name: "Dr. Brajesh Kumar Jha", affiliation: "PDEU Gandhinagar", image: "/BrajeshKumar.jpg" },
   { name: "Dr. Kunwer Singh Mathur", affiliation: "JNU Delhi", image: "/KunwerSingh.jpeg" },
   { name: "Dr. Alekh Gour", affiliation: "IIM Sirmaur", image: "/AlekhGour.jpg" },
   { name: "Dr. Sharada Nandan Raw", affiliation: "NIT Raipur", image: "/SharadaRaw.webp" },
   { name: "Dr. Damoder Reddy", affiliation: "NIT Goa", image: "/DamodarReddy.png" },
-  { name: "Dr. Ruhul Amin", affiliation: "IIIT Nayapura", image: "/ruhulamin.jpg" },
-
+  { name: "Dr. Kapil Ahuja", affiliation: "IIT Indore", image: "/KapilAhuja.jpg" },
 
 ];
 
